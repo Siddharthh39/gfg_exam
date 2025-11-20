aab4ca3f7cdf2f41792c5592b937f3552aafd8a3
Siddharth Singh <165460702+Siddharthh39@users.noreply.github.com>
2025-11-20 08:03:51 +0530

    Implement KMP pattern search algorithm

---
+++ Pattern Search KMP.py
@@ -0,0 +1,36 @@
+def computeLPSArray(pat, M, lps):
+    length = 0
+    i = 1
+    while i < M:
+        if pat[i] == pat[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+
+def KMP(pat, txt):
+    M = len(pat)
+    N = len(txt)
+    lps = [0] * M
+    computeLPSArray(pat, M, lps)
+    res = []
+    i = 0
+    j = 0
+    while i < N:
+        if pat[j] == txt[i]:
+            i += 1
+            j += 1
+        if j == M:
+            res.append(i - j)
+            j = lps[j - 1]
+        elif i < N and pat[j] != txt[i]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+    return res
